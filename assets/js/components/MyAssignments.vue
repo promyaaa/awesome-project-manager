7bfe8d8495858bf1f7a1159e4171c75f82d2c4ec
promyaaa <promyaaa@gmail.com>
2018-01-29 01:04:09 +0600

    my todo design fix

--- assets/js/components/MyAssignments.vue
+++ assets/js/components/MyAssignments.vue
@@ -10,14 +10,18 @@
         </div>
         <div class="row">
             <div class="col-12">
-                <div v-for="todo in todos" style="padding-left:20px; padding-bottom:20px">
-                    <span class="checkbox-checked-style" v-if="+todo.is_complete">
-                        <i class="fa fa-check" aria-hidden="true"></i>
-                    </span>
-                    <span class="checkbox-style" v-else></span>
-                    <router-link :to="'/projects/' + todo.projectID + '/todolists/' + todo.listID + '/todos/' + todo.ID" tag="span">
-                        {{todo.todo}}
-                    </router-link>
+                <div class="row" v-for="todo in todos" style="padding-left:20px; padding-bottom:5px">
+                    <div class="col-2 text-right">
+                        <span class="checkbox-checked-style" v-if="+todo.is_complete">
+                            <i class="fa fa-check" aria-hidden="true"></i>
+                        </span>
+                        <span class="checkbox-style" v-else></span>
+                    </div>
+                    <div class="col-10">
+                        <router-link :to="'/projects/' + todo.projectID + '/todolists/' + todo.listID + '/todos/' +todo.ID" tag="div" class="my-todo">
+                            <a>{{todo.todo}}</a>
+                        </router-link>
+                    </div>
                 </div>
             </div>
         </div>
@@ -30,6 +34,16 @@
         border-bottom: 2px solid #eee;
         margin-bottom:20px;
         padding-bottom:20px;
+    }
+    .my-todo {
+        margin-top: -3px;
+        text-decoration: none;
+        color: #ccc;
+    }
+
+    .my-todo a{
+        text-decoration: none;
+        color: #444;
     }
 </style>
 
